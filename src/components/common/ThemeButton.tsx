c95c77a3f98f6d62f5b87f9c3f6257c457e77426
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-12-18 14:50:33 +0900

    [ADD] 불필요 파일 삭제 및 글로벌 CSS, tailwindCSS 적용, 레이아웃 설정

---
+++ src/components/common/ThemeButton.tsx
@@ -0,0 +1,3 @@
+export default function ThemeButton() {
+  return;
+}
---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div>
+      <p>&copy;2023 이진호. All Rights Reserved.</p>
+    </div>
+  );
+}
