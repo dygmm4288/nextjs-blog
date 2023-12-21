59b30e0da67d6243bf67feb4e50a10726753d680
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-12-21 15:03:58 +0900

    [FIX] 기존 query string 정보 날라가지 않게 수정

--- src/components/posts/PostCategories.tsx
+++ src/components/posts/PostCategories.tsx
@@ -7,41 +7,28 @@ interface Props {
   categoriesCounter: { [key: string]: number };
 }
 
-const exampleCategoriesCounter: Record<string, number> = {
-  javascript: 1,
-  typescript: 2,
-  react: 3,
-  nextjs: 4,
-  redux: 5,
-  "redux-toolkit": 6,
-  "redux-query": 7,
-  "react-query": 8,
-  zustand: 9,
-  "functional-programming": 10,
-  nodejs: 11,
-  python: 12,
-  express: 13,
-  koa: 14,
-  graphql: 15,
-};
-
 export default function PostCategories({ categoriesCounter }: Props) {
   const searchParams = useSearchParams();
   const selectedCategory = searchParams.get("category");
+  const search = searchParams.get("search");
 
   const isSelected = (category: string) => selectedCategory === category;
 
   return (
     <ul className='flex flex-row gap-5 flex-wrap justify-center my-16'>
       {/* {Object.keys(categoriesCounter).map((category) => ( */}
-      {Object.keys(exampleCategoriesCounter).map((category) => (
+      {Object.keys(categoriesCounter).map((category) => (
         <li
           key={category}
           className={isSelected(category) ? "font-bold text-accentColor" : ""}>
           <Link
             href={
-              !isSelected(category) ? `/posts/?category=${category}` : "/posts"
-            }>{`${category}(${exampleCategoriesCounter[category]})`}</Link>
+              !isSelected(category)
+                ? `/posts?category=${category}${
+                    search ? "&search=" + search : ""
+                  }`
+                : `/posts${search ? "?search=" + search : ""}`
+            }>{`${category}(${categoriesCounter[category]})`}</Link>
         </li>
       ))}
     </ul>
