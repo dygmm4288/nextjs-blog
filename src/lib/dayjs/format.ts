a1c4c1c3d10a21ba0f2700767c3a10202c725489
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-12-22 16:50:40 +0900

    [ADD] date format short버전 기능 추가

--- src/lib/dayjs/format.ts
+++ src/lib/dayjs/format.ts
@@ -2,3 +2,6 @@ import dayjs from "dayjs";
 export default function format(day: Date | string | number) {
   return dayjs(day).format("YYYY-MM-DD");
 }
+export function formatShort(day: Date | string | number) {
+  return dayjs(day).format("YY. MM. DD.");
+}
