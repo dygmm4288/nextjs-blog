d203121b00eab2d7fa0fb622452401ca19a3509c
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-12-22 16:49:20 +0900

    [UPDATE] work detail 페이지 하단 프로젝트 기간 추가

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,4 +1,5 @@
 import { fetchBlogWorks } from "@/api/contentfulLib";
+import { formatShort } from "@/lib/dayjs/format";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,25 +9,32 @@ export default async function Works() {
   return (
     <div className='w-full h-full'>
       <ul className='p-4 grid grid-cols-2 gap-4'>
-        {works.map(({ slug, title, description, projectImg }) => (
-          <li key={slug}>
-            <Link href={`/works/${slug}`}>
-              <section className='p-4 flex flex-col gap-4'>
-                <Image
-                  className='rounded-2xl object-cover w-full h-48'
-                  src={"https:" + projectImg[0]!.src}
-                  alt='project img'
-                  width={projectImg[0]!.width}
-                  height={projectImg[0]!.height}
-                />
-                <h3 className='text-2xl font-bold'>{title}</h3>
-                <p className='text-lg opacity-50 hover:opacity-100 hover:text-accentColor'>
-                  {description}
-                </p>
-              </section>
-            </Link>
-          </li>
-        ))}
+        {works.map(
+          ({ slug, title, description, projectImg, startTime, endTime }) => (
+            <li key={slug}>
+              <Link href={`/works/${slug}`}>
+                <section className='p-4 flex flex-col gap-4'>
+                  <div className='rounded-2xl overflow-hidden'>
+                    <Image
+                      className='object-cover w-full h-48 hover:scale-105 transition-transform'
+                      src={"https:" + projectImg[0]!.src}
+                      alt='project img'
+                      width={projectImg[0]!.width}
+                      height={projectImg[0]!.height}
+                    />
+                  </div>
+                  <h3 className='text-2xl font-bold'>{title}</h3>
+                  <span>{`${formatShort(startTime)} ~ ${formatShort(
+                    endTime,
+                  )} `}</span>
+                  <p className='text-lg opacity-50 hover:opacity-100 hover:text-accentColor'>
+                    {description}
+                  </p>
+                </section>
+              </Link>
+            </li>
+          ),
+        )}
       </ul>
     </div>
   );
